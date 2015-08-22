26a4c0d868c8336a62832e2e339fc20e771c1a65
Francisco Duran <franciscodurannavarro@hotmail.com>
2015-08-22 13:48:36 +0200

    Express 00 - Introducción
    Introducción a una serie de resúmenes sobre las características de
    Express

---
+++ Back-End/Node.js/Express/00 - Express.js
@@ -0,0 +1,18 @@
+Express.js
+**********************************************************************************************
+
+Introducción
+----------------------------------------------------------------------------------------------
+Recientemente he estado trabajando y estudiando Node.js y realmente; cuando digo con Node.js realmente me refiero a Express.js aunque este está basado en Node.
+
+Express es un framework simple de base, pero que usa la potencia de Node.js; en la actualizad los frameworks de todo en uno como este Django en Python y Laravel en PHP tienen más similitudes que diferencias, las estructuras basadas en MVC junto a enrutamiento hacen que en general tengan más similitudes que diferencias, siendo los complementos y el lenguaje base lo que nos hace tomar decisiones a la hora de elegir.
+
+La gran ventaja de Express y Node, es que con otros frameworks front-end como Angular.js y otras herramientas basadas en JavaScript nos permite evitar el llamado "cambio de contexto" que se da cuando se utiliza un lenguaje de programación en el front y uno en el back genera una pérdida de tiempo al cambiar de contexto.
+
+Por eso y por la programación orientada a eventos que nos ofrece Node, el aprender a usar y dominar Express es una apuesta de futuro bastante segura.
+
+Este resumen tiene como finalizad concentrar la información conseguido en una serie de tutoriales y libros, de forma clara (al menos para mi) y con algunos ejemplos.
+
+Índice
+----------------------------------------------------------------------------------------------
+00 - Express.js - Introducción
